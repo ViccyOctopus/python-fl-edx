bc0dfdc9c795458f9d4af918ee10ed2e1cf63d72
Victoria Arzumanova <victoriaarzumanova123@gmail.com>
2019-09-17 12:10:46 -0400

    done with salestax.py and remembered how to add and subtract

--- salestax.py
+++ salestax.py
@@ -4,8 +4,9 @@ print("What is the original price of the product?")
 oriPrice = input()
 intPrice = int(oriPrice)
 
-totTax = (intPrice*0.006)
-endPrice = (intPrice - totTax)
-Price = str(endPrice)
+totTax = (intPrice*1.06)
+endPrice1 = (totTax - intPrice)
+endPrice2 = (endPrice1 + intPrice)
+Price = str(endPrice2)
 
 print("Your total price after taxes would be " + Price + ".")
